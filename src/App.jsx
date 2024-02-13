faecf9646f5728a600039b3b904dd753a854d332
Matthew Sanner <matthewsannerdev@gmail.com>
2024-02-13 01:31:16 +0000

    personalized siteProps

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Matthew Sanner",
+  title: "Web Developer & IT Consultant",
+  email: "matthewsannerdev@gmail.com",
+  gitHub: "matthewsanner",
+  instagram: "",
+  linkedIn: "matthew-sanner2",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "MattSannerDev",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
